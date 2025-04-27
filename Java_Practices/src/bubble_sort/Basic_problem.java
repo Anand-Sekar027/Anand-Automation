0d0bdbe8df5b2da0acda57870b21fb4aa5e1169e
Anand <anandsekar027@gmail.com>
2025-04-28 02:02:38 +0530

    Added bubble sort

---
+++ Java_Practices/src/bubble_sort/Basic_problem.java
@@ -0,0 +1,31 @@
+package bubble_sort;
+
+import java.util.Arrays;
+
+public class Basic_problem {
+
+	public static void main(String[] args) {
+		/* Push the maximum to the last
+		 * by using adjacent swaps
+		 */
+		int[] arr = {99, 77, 55, 8, 14, 186, 1};
+		bubble_sort_1(arr);		
+	}
+	
+	public static void bubble_sort_1(int[] arr) {
+		int n = arr.length;
+		for(int i=0; i<n-1; i++) {
+			for(int j=0; j<n-1-i; j++) {
+				if(arr[j]>arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+			//System.out.println("Sorted Array : "+ Arrays.toString(arr));
+		}
+		System.out.println("Sorted Array : "+ Arrays.toString(arr));
+		
+	}
+
+}
--- Java_Practices/src/selection_sort/Basic_programe.java
+++ Java_Practices/src/selection_sort/Basic_programe.java
@@ -4,6 +4,10 @@ import java.util.Arrays;
 
 public class Basic_programe {
 
+	/* Take i index as minimum and compare rest for values and find minimum value and their index.
+	 * Swap minimum value to i and increase i till n-1 .
+	 * no need to check for last value its already largest number
+	 */
 	public static void main(String[] args) {
 		int[] arr= {15, 55, 4, 98, 35};
 		programe_one(arr);
@@ -18,14 +22,14 @@ public class Basic_programe {
 			for(int j =i; j<n-1; j++) {				
 				if (int_arr[j] < int_arr[min_index]) {					
 					min_index = j; // Find minimum value's index
-					System.out.println("Mininum value = "+int_arr[j]+ " & index -> "+ j );
-					System.out.println("i - value = "+int_arr[i]+ " & index -> "+ i );
+					//System.out.println("Mininum value = "+int_arr[j]+ " & index -> "+ j );
+					//System.out.println("i - value = "+int_arr[i]+ " & index -> "+ i );
 				}
 				// swap value of minimum to array of i
 				int temp = int_arr[min_index];
 				int_arr[min_index] = int_arr[i];
 				int_arr[i] = temp;
-				System.out.println("Sorted Array @ i="+ i + " --> " + Arrays.toString(int_arr));
+				//System.out.println("Sorted Array @ i="+ i + " --> " + Arrays.toString(int_arr));
 			}
 		}
 		
